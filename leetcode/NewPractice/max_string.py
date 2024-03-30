773c827a3d09f489dbf06c176affb068657de18f
utshav2008 <utshav2008@gmail.com>
2024-03-30 04:34:37 +0000

    max string problem

---
+++ leetcode/NewPractice/max_string.py
@@ -0,0 +1,12 @@
+def find_max_string(s):
+    l = 0
+    max_length = 0
+    string_set = set()
+    for r in range(len(s)):
+        if s[r] in string_set:
+            
+             
+        
+
+
+find_max_string('abcabbad')
