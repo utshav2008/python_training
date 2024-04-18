7671c3268eaf6745e34d9ac5054e989d2616aa48
utshav2008 <utshav2008@gmail.com>
2024-04-18 08:59:25 +0000

    adding dictionary

--- leetcode/NewPractice/sort_dict.py
+++ leetcode/NewPractice/sort_dict.py
@@ -1,5 +1,5 @@
 myDict = {'ravi': 10, 'rajnish': 9,
-        'sanjeev': 15, 'yash': 2, 'suraj': 32}
+        'sanjeev': 15, 'yash': 2, 'suraj': 32, 'ram':24}
 
 
 sorted_dict = dict(sorted(myDict.items(), key = lambda x: x[0]))
