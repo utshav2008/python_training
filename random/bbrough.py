458ab89a451b428404a5ba7eda4d60b8db80ea69
utshav2008 <utshav2008@gmail.com>
2025-02-05 13:43:03 +0000

    practice

--- random/bbrough.py
+++ random/bbrough.py
@@ -1,9 +1,9 @@
 class Sequencer:
     def __init__(self):
-        self.expected_seq = 0  # The next sequence number we expect
+        self.expected_seq = 1  # The next sequence number we expect
         self.buffer = {}  # Dictionary to store out-of-order packets
 
-    def handle(self, data: str, seq_num: int):
+    def handle(self, data, seq_num):
         if seq_num == self.expected_seq:
             # Print the in-order packet immediately
             print(data)
@@ -18,9 +18,18 @@ class Sequencer:
             self.buffer[seq_num] = data
 
 
+# sequencer = Sequencer()
+
+# stream = [(1, 'aaa'), (2, 'bbb'), (4, 'ddd'), (3, 'ccc'), (6, 'fff'), (5, 'eee')]
+
+# for seq_num, data in stream:
+#     sequencer.handle(data, seq_num)
+#     print(sequencer.buffer)
+
 sequencer = Sequencer()
 
 stream = [(1, 'aaa'), (2, 'bbb'), (4, 'ddd'), (3, 'ccc'), (6, 'fff'), (5, 'eee')]
 
 for seq_num, data in stream:
     sequencer.handle(data, seq_num)
+    print(sequencer.buffer)
