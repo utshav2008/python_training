eb37ab4f2e150dcf47b87a7b10bffdf455e4cdb0
utshav2008 <utshav2008@gmail.com>
2025-02-17 01:50:04 +0000

    new comits

---
+++ leetcode/leetcode2025/sequencer.py
@@ -0,0 +1,35 @@
+class Sequencer:
+    def __init__(self):
+        self.expected_seq = 1  # The next sequence number we expect
+        self.buffer = {}  # Dictionary to store out-of-order packets
+
+    def handle(self, data, seq_num):
+        if seq_num == self.expected_seq:
+            # Print the in-order packet immediately
+            print(data)
+            self.expected_seq += 1
+            
+            # Check the buffer for the next expected packets
+            while self.expected_seq in self.buffer:
+                print(self.buffer.pop(self.expected_seq))
+                self.expected_seq += 1
+        elif seq_num > self.expected_seq:
+            # Buffer out-of-order packets
+            self.buffer[seq_num] = data
+
+
+# sequencer = Sequencer()
+
+# stream = [(1, 'aaa'), (2, 'bbb'), (4, 'ddd'), (3, 'ccc'), (6, 'fff'), (5, 'eee')]
+
+# for seq_num, data in stream:
+#     sequencer.handle(data, seq_num)
+#     print(sequencer.buffer)
+
+sequencer = Sequencer()
+
+stream = [(1, 'aaa'), (2, 'bbb'), (4, 'ddd'), (3, 'ccc'), (6, 'fff'), (5, 'eee')]
+
+for seq_num, data in stream:
+    sequencer.handle(data, seq_num)
+    print(sequencer.buffer)
